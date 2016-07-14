3b6cef0e1d3918fe421e2dd8b0ed425e626d6235
Mateus <mateussabino87@gmail.com>
2016-07-14 12:47:32 -0300

    refactor: add clasroom helper methods

--- spec/helpers/classrooms_helper_spec.rb
+++ spec/helpers/classrooms_helper_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ClassroomsHelper. For example:
-#
-# describe ClassroomsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ClassroomsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#students_map' do
+    let!(:students) { create_list(:student, 2) }
+    let(:expected_result) do
+      [
+        [students.first.name, students.first.id],
+        [students.second.name, students.second.id]
+      ]
+    end
+
+    it 'returns a students map' do
+      expect(students_map).to match_array(expected_result)
+    end
+  end
+
+  describe '#courses_map' do
+    let!(:courses) { create_list(:course, 2) }
+    let(:expected_result) do
+      [
+        [courses.first.name, courses.first.id],
+        [courses.second.name, courses.second.id]
+      ]
+    end
+
+    it 'returns a courses map' do
+      expect(courses_map).to match_array(expected_result)
+    end
+  end
+
 end
